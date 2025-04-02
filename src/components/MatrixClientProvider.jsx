cb4429a9632400479e08c714b1dab6d75fc20cd7
TitanSolve <asakurar31@gmail.com>
2025-04-01 22:05:20 -0700

    Edit: MuiCapabilitiesGuard

--- src/components/MatrixClientProvider.jsx
+++ src/components/MatrixClientProvider.jsx
@@ -1,32 +1,10 @@
-import React, { useEffect, useState, Suspense, ReactElement } from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { EventDirection, WidgetEventCapability} from "matrix-widget-api";
-import { WidgetApiImpl, WidgetParameter } from '@matrix-widget-toolkit/api';
+import React, { useEffect, useState, ReactElement } from "react";
 import { useWidgetApi } from '@matrix-widget-toolkit/react';
-import { MuiThemeProvider, MuiWidgetApiProvider} from '@matrix-widget-toolkit/mui';
 import { Tabs, Tab, Box } from "@mui/material";
 import { motion, AnimatePresence } from "framer-motion";
 import { map } from 'rxjs';
 import NFTs from "../pages/NFTs";
 import Offers from "../pages/Offers";
-
-
-// Initiate the widget API on startup. The Client will initiate
-// the connection with `capabilities` and we need to make sure
-// that the message doesn't get lost while we are initiating React.
-const widgetApiPromise = WidgetApiImpl.create({
-  // You can specify which capabilities should be requested at startup. One
-  // can also request capabilities after the application started.
-  capabilities: [
-    WidgetEventCapability.forStateEvent(
-      EventDirection.Receive,
-      'm.room.name',
-    ),
-  ],
-});
-
-
-
 
 const MatrixClientProvider = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -64,51 +42,33 @@ const MatrixClientProvider = () => {
   };
 
   return (
-    <BrowserRouter>
-      <MuiThemeProvider>
-        <Suspense fallback={<></>}>
-          <MuiWidgetApiProvider
-            widgetApiPromise={widgetApiPromise}
-            widgetRegistration={{
-              name: 'P2P-NFT-Widget',
-              type: 'com.example.clock',
-              data: { title: 'P2P-NFT-Widget' },
-              // Device ID is required for the WelcomePage example
-              requiredParameters: [WidgetParameter.DeviceId],
-            }}
+    <Box sx={{ width: "100%", borderRadius: 2, boxShadow: 1 }}>
+      <Tabs
+        value={selectedIndex}
+        onChange={(event, newIndex) => setSelectedIndex(newIndex)}
+        variant="fullWidth"
+        textColor="primary"
+        indicatorColor="primary"
+      >
+        <Tab label="NFTs" />
+        <Tab label="Offers" />
+      </Tabs>
+      <Box sx={{ p: 2, position: "relative", overflow: "hidden" }}>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={selectedIndex}
+            variants={panelVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ duration: 0.4, ease: "easeInOut" }}
           >
-            <Box sx={{ width: "100%", borderRadius: 2, boxShadow: 1 }}>
-              <Tabs
-                value={selectedIndex}
-                onChange={(event, newIndex) => setSelectedIndex(newIndex)}
-                variant="fullWidth"
-                textColor="primary"
-                indicatorColor="primary"
-              >
-                <Tab label="NFTs" />
-                <Tab label="Offers" />
-              </Tabs>
-              <Box sx={{ p: 2, position: "relative", overflow: "hidden" }}>
-                <AnimatePresence mode="wait">
-                  <motion.div
-                    key={selectedIndex}
-                    variants={panelVariants}
-                    initial="hidden"
-                    animate="visible"
-                    exit="exit"
-                    transition={{ duration: 0.4, ease: "easeInOut" }}
-                  >
-                    {selectedIndex === 0 ? <NFTs /> : <Offers />}
-                  </motion.div>
-                </AnimatePresence>
-              </Box>
-            </Box>
+            {selectedIndex === 0 ? <NFTs /> : <Offers />}
+          </motion.div>
+        </AnimatePresence>
+      </Box>
+    </Box>
 
-
-          </MuiWidgetApiProvider>
-        </Suspense>
-      </MuiThemeProvider>
-    </BrowserRouter>
   );
 };
 
