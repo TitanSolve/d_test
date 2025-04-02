1ef054acbd42c0767656275d7d30af01b6471eee
TitanSolve <asakurar31@gmail.com>
2025-04-01 23:19:38 -0700

    Edit: index.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,41 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 import { EventDirection, WidgetEventCapability } from "matrix-widget-api";
-import { MuiCapabilitiesGuard, MuiThemeProvider, MuiWidgetApiProvider} from '@matrix-widget-toolkit/mui';
+import { MuiCapabilitiesGuard, MuiThemeProvider, MuiWidgetApiProvider } from '@matrix-widget-toolkit/mui';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { WidgetApiImpl, WidgetParameter } from '@matrix-widget-toolkit/api';
 import MatrixClientProvider from './components/MatrixClientProvider';
 
-
-// Initiate the widget API on startup. The Client will initiate
-// the connection with `capabilities` and we need to make sure
-// that the message doesn't get lost while we are initiating React.
-const widgetApiPromise = WidgetApiImpl.create({
-  // You can specify which capabilities should be requested at startup. One
-  // can also request capabilities after the application started.
-  capabilities: [
-    WidgetEventCapability.forStateEvent(
-      EventDirection.Receive,
-      'm.room.name',
-    ),
-  ],
-});
-
-function App() {
+function App({ widgetApiPromise }) {
   return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <MuiThemeProvider>
-          <Suspense fallback={<></>}>
-            <MuiWidgetApiProvider
-              widgetApiPromise={widgetApiPromise}
-              widgetRegistration={{
-                name: 'P2P-NFT-Widget',
-                type: 'com.example.clock',
-                data: { title: 'P2P-NFT-Widget' },
-                // Device ID is required for the WelcomePage example
-                requiredParameters: [WidgetParameter.DeviceId],
-              }}
-            >
-              <MuiCapabilitiesGuard
-                capabilities={[
-                  WidgetEventCapability.forRoomEvent(
-                    EventDirection.Receive,
-                    'net.nordeck.throw_dice',
-                  ),
-                ]}
-              >
+    <BrowserRouter>
+      <MuiThemeProvider>
+        <Suspense fallback={<></>}>
+          <MuiWidgetApiProvider
+            widgetApiPromise={widgetApiPromise}
+            widgetRegistration={{
+              name: 'P2P-NFT-Widget',
+              type: 'com.example.clock',
+              data: { title: 'P2P-NFT-Widget' },
+              // Device ID is required for the WelcomePage example
+              requiredParameters: [WidgetParameter.DeviceId],
+            }}
+          >
+            {/* <MuiCapabilitiesGuard
+              capabilities={[
+                WidgetEventCapability.forRoomEvent(
+                  EventDirection.Receive,
+                  'net.nordeck.throw_dice',
+                ),
+              ]}
+            > */}
 
-                <MatrixClientProvider />
+              <MatrixClientProvider />
 
-              </MuiCapabilitiesGuard>
-            </MuiWidgetApiProvider>
-          </Suspense>
-        </MuiThemeProvider>
-      </BrowserRouter>
-    </React.StrictMode>
+            {/* </MuiCapabilitiesGuard> */}
+          </MuiWidgetApiProvider>
+        </Suspense>
+      </MuiThemeProvider>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { WidgetApiImpl } from '@matrix-widget-toolkit/api';
+import { EventDirection, WidgetEventCapability } from 'matrix-widget-api';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const widgetApiPromise = WidgetApiImpl.create({
+    // You can specify which capabilities should be requested at startup. One
+    // can also request capabilities after the application started.
+    capabilities: [
+      WidgetEventCapability.forStateEvent(
+        EventDirection.Receive,
+        'm.room.name',
+      ),
+    ],
+  });
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App widgetApiPromise={widgetApiPromise} />
+  </React.StrictMode>,
+);
