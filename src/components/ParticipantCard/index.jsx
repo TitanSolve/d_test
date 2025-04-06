f8ae5b968ee2fc55af4ba60b73f44ee9b183e3a8
TitanSolve <asakurar31@gmail.com>
2025-04-06 01:05:41 -0700

    fix: fix typeError issue 29

--- src/components/ParticipantCard/index.jsx
+++ src/components/ParticipantCard/index.jsx
@@ -75,7 +75,7 @@ const ParticipantCard = ({ index, myNftData, wgtParameters, getImageData }) => {
 
         <div className="flex items-center justify-between mt-4 space-x-4">
           <button
-            lassName={`swiper-button-prev-${index} bg-gray-800 hover:bg-gray-700 text-white p-2 md:p-3 rounded-full shadow-lg transition-transform transform hover:scale-110 focus:ring-4 focus:ring-purple-500`}
+            className={`swiper-button-prev-${index} bg-gray-800 hover:bg-gray-700 text-white p-2 md:p-3 rounded-full shadow-lg transition-transform transform hover:scale-110 focus:ring-4 focus:ring-purple-500`}
           >
             <ChevronLeft size={24} />
           </button>
