05fde92e7fd0a8cab39a01cf54dd444f9b28a34b
TitanSolve <asakurar31@gmail.com>
2025-04-07 14:33:12 -0700

    fix: fix typeError issue 83

--- src/components/ParticipantCard/index.jsx
+++ src/components/ParticipantCard/index.jsx
@@ -5,6 +5,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/effect-coverflow";
 import './index.css';
+import nft_pic from "../../assets/nft.png";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 // import { Modal, Select, Dropdown, Menu, Input, Button, Switch, Typography, Space } from "antd";
 import {
@@ -190,8 +191,18 @@ const ParticipantCard = ({ index, myNftData, wgtParameters, getImageData }) => {
             }
           >
             {state.selectedGroup &&
-              state.selectedGroup.map((nftItem, idx) => (
-                <NFTCard key={idx} myNftData={[nftItem]} handleClick={() => {}} />
+              state.selectedGroup.nfts.map((nftItem, idx) => (
+                // <NFTCard key={idx} myNftData={[nftItem]} handleClick={() => {}} />
+                <div className="transform hover:scale-105 transition-transform duration-300 border p-2 rounded-lg shadow-md bg-gradient-to-br from-blue-200 to-purple-300 text-gray-800 font-semibold text-center cursor-pointer">
+                  <img
+                    // src={nft_pic}
+                    src={nftItem.imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")}
+                    onError={(e) => { e.target.onerror = null; e.target.src = nft_pic; }}
+                    alt="NFT"
+                    // loading="lazy"
+                    className="mx-auto rounded-lg object-cover shadow-md w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 xl:w-64 xl:h-64"
+                  />
+                </div>
               ))}
           </Carousel>
         </Box>
