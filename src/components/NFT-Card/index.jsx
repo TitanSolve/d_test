f7e3002cc410a331d5aa23e4aa79c4f438304783
TitanSolve <asakurar31@gmail.com>
2025-04-07 12:01:47 -0700

    fix: fix typeError issue 71

--- src/components/NFT-Card/index.jsx
+++ src/components/NFT-Card/index.jsx
@@ -2,19 +2,20 @@ import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import API_URLS from "../../config";
 import nft_pic from "../../assets/nft.png";
-import { useInView } from 'react-intersection-observer';
+ import { useInView } from 'react-intersection-observer';
 
 const NFTCard = ({ myNftData }) => {
-    const { ref, inView } = useInView({ triggerOnce: true });
-    
+    // const { ref, inView } = useInView({ triggerOnce: true });
+    console.log("NFTCard", myNftData);
+
     return (
         <>
-            <div ref={ref} className="transform hover:scale-105 transition-transform duration-300 border p-2 rounded-lg shadow-md bg-gradient-to-br from-blue-200 to-purple-300 text-gray-800 font-semibold text-center cursor-pointer">
+            <div className="transform hover:scale-105 transition-transform duration-300 border p-2 rounded-lg shadow-md bg-gradient-to-br from-blue-200 to-purple-300 text-gray-800 font-semibold text-center cursor-pointer">
                 <img
                     src={myNftData.imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")}
                     onError={(e) => { e.target.onerror = null; e.target.src = nft_pic; }}
                     alt="NFT"
-                    loading="lazy"
+                    // loading="lazy"
                     className="mx-auto rounded-lg object-cover shadow-md w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 xl:w-64 xl:h-64"
                 />
             </div>
