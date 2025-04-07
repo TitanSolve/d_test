a202c31b128a186a30145d1dba6483f2de23726a
TitanSolve <asakurar31@gmail.com>
2025-04-07 14:16:46 -0700

    fix: fix typeError issue 79

--- src/components/NFT-Card/index.jsx
+++ src/components/NFT-Card/index.jsx
@@ -6,14 +6,15 @@ import nft_pic from "../../assets/nft.png";
 
 const NFTCard = ({ myNftData, handleClick  }) => {
     // const { ref, inView } = useInView({ triggerOnce: true });
-    // console.log("NFTCard", myNftData);
+    console.log("NFTCard", myNftData);
 
     return (
         <>
             <div onClick={handleClick} className="transform hover:scale-105 transition-transform duration-300 border p-2 rounded-lg shadow-md bg-gradient-to-br from-blue-200 to-purple-300 text-gray-800 font-semibold text-center cursor-pointer">
                 <img
-                    src={myNftData[0].imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")}
-                    onError={(e) => { e.target.onerror = null; e.target.src = nft_pic; }}
+                    src={nft_pic}
+                    // src={myNftData[0].imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")}
+                    // onError={(e) => { e.target.onerror = null; e.target.src = nft_pic; }}
                     alt="NFT"
                     // loading="lazy"
                     className="mx-auto rounded-lg object-cover shadow-md w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 xl:w-64 xl:h-64"
