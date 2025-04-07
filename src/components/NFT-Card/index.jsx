5fd5d4f316e4f9307f606edf03a9790cd10a39f3
TitanSolve <asakurar31@gmail.com>
2025-04-07 06:28:35 -0700

    fix: fix typeError issue 60

--- src/components/NFT-Card/index.jsx
+++ src/components/NFT-Card/index.jsx
@@ -2,11 +2,14 @@ import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import API_URLS from "../../config";
 import nft_pic from "../../assets/nft.png";
-// className="w-full max-w-xs md:max-w-sm h-auto rounded-lg mx-auto object-cover"
+import { useInView } from 'react-intersection-observer';
+
 const NFTCard = ({ myNftData }) => {
+    const { ref, inView } = useInView({ triggerOnce: true });
+    
     return (
         <>
-            <div className="transform hover:scale-105 transition-transform duration-300 border p-2 rounded-lg shadow-md bg-gradient-to-br from-blue-200 to-purple-300 text-gray-800 font-semibold text-center cursor-pointer">
+            <div ref={ref} className="transform hover:scale-105 transition-transform duration-300 border p-2 rounded-lg shadow-md bg-gradient-to-br from-blue-200 to-purple-300 text-gray-800 font-semibold text-center cursor-pointer">
                 <img
                     src={myNftData.imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")}
                     onError={(e) => { e.target.onerror = null; e.target.src = nft_pic; }}
