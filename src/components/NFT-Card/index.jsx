ad71aecd815fc853dc9044e1a15a0c881da853ab
TitanSolve <asakurar31@gmail.com>
2025-04-08 20:33:12 -0700

    fix: fix typeError issue 105

--- src/components/NFT-Card/index.jsx
+++ src/components/NFT-Card/index.jsx
@@ -8,6 +8,9 @@ const NFTCard = ({ myNftData, isGroup, isImgOnly }) => {
     const imageUrl = (isGroup ? myNftData.nfts[0].imageURI : myNftData.imageURI)
         .replace("ipfs://", "https://ipfs.io/ipfs/");
 
+        console.log("myNftData--------->", myNftData);
+        console.log("imageUrl--------->", imageUrl);
+
         return (
             <div className={`mx-auto transform transition-transform duration-300 text-gray-800 font-semibold text-center ${!isImgOnly ? 'cursor-pointer hover:scale-105 from-blue-200 to-purple-300 bg-gradient-to-br border p-2 rounded-lg shadow-md' : ''}`}>
                 {!imgLoaded && (
