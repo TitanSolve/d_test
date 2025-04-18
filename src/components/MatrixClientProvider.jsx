6a823b06c2a9c39c3e4011de90dcc5a345f3d25a
TrustBrain <web.jackie1992@hotmail.com>
2025-04-18 11:13:30 +0400

    fix:fifx typeError issue 228

--- src/components/MatrixClientProvider.jsx
+++ src/components/MatrixClientProvider.jsx
@@ -12,8 +12,8 @@ import nft_default_pic from "../assets/nft.png";
 import { WidgetApi } from "matrix-widget-api";
 import { Tooltip } from "@mui/material";
 import { ThemeProvider, useTheme } from "../context/ThemeContext";
-import { Buffer } from 'buffer'
 import "./index.css"
+import {Buffer} from "buffer";
 
 const hexToAscii = (str) => {
   var hexString = str?.toString();
