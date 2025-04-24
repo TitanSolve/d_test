87162b9253fccb42ffbe8458f761da9afc2ff610
TitanSolve <asakurar31@gmail.com>
2025-04-24 01:59:50 -0700

    transaction test 16

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -164,7 +164,7 @@ const Offers = (membersList, myWalletAddress) => {
 
   useEffect(() => {
     console.log("Offers->useEffect", membersList, myWalletAddress);
-    if( myWalletAddress === "" || membersList.membersList.isEmpty() ) return;
+    if( myWalletAddress === "" || membersList.length < 1 ) return;
     refreshOffers();
   }, [membersList, myWalletAddress]);
 
