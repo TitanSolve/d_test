8ec1a69f8f8379cf78eac91d9dce52894fed6c29
TitanSolve <asakurar31@gmail.com>
2025-05-12 10:27:38 -0700

    fix performance issue 94

--- src/components/OfferReceivedCard/index.jsx
+++ src/components/OfferReceivedCard/index.jsx
@@ -4,6 +4,7 @@ import API_URLS from "../../config";
 import { Button } from "antd";
 import TransactionModal from "../TransactionModal";
 import NFTMessageBox from "../NFTMessageBox";
+import LoadingOverlayForCard from "../LoadingOverlayForCard";
 
 const OfferReceivedCard = ({
   sellOffers,
@@ -12,7 +13,7 @@ const OfferReceivedCard = ({
   onAction,
   myWalletAddress,
   refreshSellOffers,
-  updateUsersNFTs
+  updateUsersNFTs,
 }) => {
   const [qrCodeUrl, setQrCodeUrl] = useState("");
   const [websocketUrl, setWebsocketUrl] = useState("");
@@ -26,6 +27,7 @@ const OfferReceivedCard = ({
   const [messageBoxText, setMessageBoxText] = useState("");
   const [roomMessage, setRommMessage] = useState("");
   const [sendRoomMsg, setSendRoomMsg] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (sendRoomMsg && roomMessage !== "") {
@@ -45,20 +47,30 @@ const OfferReceivedCard = ({
     console.log("Accpet clicked for item:", buyOffer);
     console.log("SellOffer--->", madeOffers);
 
+    setIsLoading(true);
     setTransactionStatus("");
 
     let isOfferFound = false;
     let sellOfferIndex = "";
-    let brokerFee = ((buyOffer.offer.amount * 1 - 12) / 1.01 * 0.01).toFixed(0);
+    let brokerFee = (((buyOffer.offer.amount * 1 - 12) / 1.01) * 0.01).toFixed(
+      0
+    );
     let sellOfferOwnder = "";
     for (const offer of madeOffers) {
       console.log("offer--->", offer);
       if (offer.nft.nftokenID === buyOffer.nft.nftokenID) {
         isOfferFound = true;
         sellOfferIndex = offer.offer.offerId;
-        brokerFee = ((buyOffer.offer.amount * 1 - 12) / 1.01 * 0.01).toFixed(0);
+        brokerFee = (((buyOffer.offer.amount * 1 - 12) / 1.01) * 0.01).toFixed(
+          0
+        );
         sellOfferOwnder = offer.offer.offerOwner;
-        console.log("brokerFee--->", brokerFee, buyOffer.offer.amount, offer.offer.amount);
+        console.log(
+          "brokerFee--->",
+          brokerFee,
+          buyOffer.offer.amount,
+          offer.offer.amount
+        );
         break;
       }
     }
@@ -91,6 +103,7 @@ const OfferReceivedCard = ({
         console.log("response", response);
 
         const data = await response.json();
+        setIsLoading(false);
         if (data) {
           console.log(data, "data");
           if (data.result.meta.TransactionResult === "tesSUCCESS") {
@@ -115,7 +128,9 @@ const OfferReceivedCard = ({
       console.log("No matching offer found for the selected NFT.");
       let sellAmount = "0";
       sellAmount = (
-        (buyOffer.offer.amount * 1 - 12) / 1.01 / 1000000
+        (buyOffer.offer.amount * 1 - 12) /
+        1.01 /
+        1000000
       ).toString();
 
       const payload = {
@@ -158,6 +173,7 @@ const OfferReceivedCard = ({
   async function onCancelOffer() {
     console.log("Cancel clicked for item:", buyOffer);
     setTransactionStatus("");
+    setIsLoading(true);
     const requestBody = {
       account: buyOffer.offer.offerOwner,
       offerId: buyOffer.offer.offerId,
@@ -177,6 +193,7 @@ const OfferReceivedCard = ({
       }
 
       const data = await response.json();
+      setIsLoading(false);
       if (data) {
         console.log(data.refs, "data refs");
         onAction();
@@ -198,7 +215,9 @@ const OfferReceivedCard = ({
       if (offer.nft.nftokenID === buyOffer.nft.nftokenID) {
         isOfferFound = true;
         sellOfferIndex = offer.offer.offerId;
-        brokerFee = ((buyOffer.offer.amount * 1 - 12) / 1.01 * 0.01).toFixed(0);
+        brokerFee = (((buyOffer.offer.amount * 1 - 12) / 1.01) * 0.01).toFixed(
+          0
+        );
         break;
       }
     }
@@ -245,6 +264,7 @@ const OfferReceivedCard = ({
         console.error("Error during fetch:", error);
       }
     }
+    setIsLoading(false);
   }
 
   async function refreshSellOfferAndAccept() {
@@ -298,67 +318,74 @@ const OfferReceivedCard = ({
   }
 
   return (
-    <div className="flex flex-col sm:flex-row items-center bg-white dark:bg-[#1a1d21] p-5 rounded-2xl shadow-xl w-full max-w-3xl border border-gray-200 dark:border-gray-700 gap-1 transition-all duration-300">
-      <div className="w-full sm:w-auto flex justify-center">
-        <img
-          src={buyOffer.nft.imageURI}
-          alt="NFT Preview"
-          className="w-32 h-32 sm:w-40 sm:h-40 rounded-xl object-cover shadow-md border border-gray-300 dark:border-gray-600"
-        />
-      </div>
+    <>
+      {isLoading ? (
+        <LoadingOverlayForCard />
+      ) : (
+        <div className="flex flex-col sm:flex-row items-center bg-white dark:bg-[#1a1d21] p-5 rounded-2xl shadow-xl w-full max-w-3xl border border-gray-200 dark:border-gray-700 gap-1 transition-all duration-300">
+          <div className="w-full sm:w-auto flex justify-center">
+            <img
+              src={buyOffer.nft.imageURI}
+              alt="NFT Preview"
+              className="w-32 h-32 sm:w-40 sm:h-40 rounded-xl object-cover shadow-md border border-gray-300 dark:border-gray-600"
+            />
+          </div>
 
-      <div className="flex flex-col text-center sm:text-left gap-1 flex-grow">
-        <span className="font-semibold text-gray-900 dark:text-white text-lg sm:text-xl truncate">
-          {buyOffer.nft.name}
-        </span>
-      </div>
+          <div className="flex flex-col text-center sm:text-left gap-1 flex-grow">
+            <span className="font-semibold text-gray-900 dark:text-white text-lg sm:text-xl truncate">
+              {buyOffer.nft.name}
+            </span>
+          </div>
 
-      <div className="flex flex-col sm:items-end text-center sm:text-right w-full sm:w-auto gap-1">
-        <div>
-          <span className="text-xl font-bold text-gray-900 dark:text-white">
-            Amount: {(((buyOffer.offer.amount * 1 - 12) / 1.01) / 1000000).toFixed(6)}
-            {/* Amount: {(((buyOffer.amount * 1 - 12) * 0.99) / 1000000).toFixed(6)} */}
-          </span>
-          <p className="text-gray-500 dark:text-gray-400 text-sm">
-            Received Offer
-          </p>
+          <div className="flex flex-col sm:items-end text-center sm:text-right w-full sm:w-auto gap-1">
+            <div>
+              <span className="text-xl font-bold text-gray-900 dark:text-white">
+                Amount:{" "}
+                {((buyOffer.offer.amount * 1 - 12) / 1.01 / 1000000).toFixed(6)}
+                {/* Amount: {(((buyOffer.amount * 1 - 12) * 0.99) / 1000000).toFixed(6)} */}
+              </span>
+              <p className="text-gray-500 dark:text-gray-400 text-sm">
+                Received Offer
+              </p>
+            </div>
+            <div className="flex flex-col sm:flex-row items-center justify-between w-full sm:w-auto space-y-4 sm:space-y-0 sm:space-x-4">
+              <Button
+                type="primary"
+                onClick={onAcceptOffer}
+                block
+                style={{ borderRadius: "6px", alignItems: "center" }}
+                className="dark:bg-green-600 dark:hover:bg-green-500"
+                // className="w-full sm:w-auto bg-red-500 text-white px-4 sm:px-5 py-2 rounded-lg hover:bg-red-600 transition shadow-md text-center">
+              >
+                Accpet
+              </Button>
+              <Button
+                type="primary"
+                onClick={onCancelOffer}
+                block
+                style={{ borderRadius: "6px", alignItems: "center" }}
+                className="dark:bg-red-600 dark:hover:bg-red-500"
+                // className="w-full sm:w-auto bg-red-500 text-white px-4 sm:px-5 py-2 rounded-lg hover:bg-red-600 transition shadow-md text-center">
+              >
+                Reject
+              </Button>
+            </div>
+          </div>
+          <TransactionModal
+            isOpen={isQrModalVisible}
+            onClose={() => setIsQrModalVisible(false)}
+            qrCodeUrl={qrCodeUrl}
+            transactionStatus={transactionStatus}
+          />
+          <NFTMessageBox
+            isOpen={isMessageBoxVisible}
+            onClose={handleCloseMessageBox}
+            type={messageBoxType}
+            message={messageBoxText}
+          />
         </div>
-        <div className="flex flex-col sm:flex-row items-center justify-between w-full sm:w-auto space-y-4 sm:space-y-0 sm:space-x-4">
-          <Button
-            type="primary"
-            onClick={onAcceptOffer}
-            block
-            style={{ borderRadius: "6px", alignItems: "center" }}
-            className="dark:bg-green-600 dark:hover:bg-green-500"
-            // className="w-full sm:w-auto bg-red-500 text-white px-4 sm:px-5 py-2 rounded-lg hover:bg-red-600 transition shadow-md text-center">
-          >
-            Accpet
-          </Button>
-          <Button
-            type="primary"
-            onClick={onCancelOffer}
-            block
-            style={{ borderRadius: "6px", alignItems: "center" }}
-            className="dark:bg-red-600 dark:hover:bg-red-500"
-            // className="w-full sm:w-auto bg-red-500 text-white px-4 sm:px-5 py-2 rounded-lg hover:bg-red-600 transition shadow-md text-center">
-          >
-            Reject
-          </Button>
-        </div>
-      </div>
-      <TransactionModal
-        isOpen={isQrModalVisible}
-        onClose={() => setIsQrModalVisible(false)}
-        qrCodeUrl={qrCodeUrl}
-        transactionStatus={transactionStatus}
-      />
-      <NFTMessageBox
-        isOpen={isMessageBoxVisible}
-        onClose={handleCloseMessageBox}
-        type={messageBoxType}
-        message={messageBoxText}
-      />
-    </div>
+      )}
+    </>
   );
 };
 
