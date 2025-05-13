f792c79aa73c27a30874f65e9a52740c2a49395e
TitanSolve <asakurar31@gmail.com>
2025-05-13 08:21:09 -0700

    fix performance issue 133

--- src/components/MatrixClientProvider.jsx
+++ src/components/MatrixClientProvider.jsx
@@ -106,10 +106,15 @@ const MatrixClientProvider = () => {
   const [myOwnWalletAddress, setMyWalletAddress] = useState("");
   const [isRefreshing, setIsRefreshing] = useState(0);
   const [incomingOffer, setIncomingOffer] = useState(null);
-  const [cancelledOffer, setCancelledOffer] = useState(null);
   const [subscribedUsers, setSubscribedUsers] = useState([]);
   const [client, setClient] = useState(null);
   const xrpl = require("xrpl");
+
+  // useEffect(() => {
+  //   return () => {
+  //     client.disconnect();
+  //   };
+  // }, []);
 
   useEffect(() => {
     const loadData = async () => {
@@ -441,17 +446,15 @@ const MatrixClientProvider = () => {
   }
 
   useEffect(() => {
-    if (
-      !client ||
-      !myNftData.length ||
-      !myOwnWalletAddress ||
-      !subscribedUsers.length
-    )
-      return;
+    if (!client || !myNftData.length || !myOwnWalletAddress || !subscribedUsers.length ) return;
 
     console.log("------------------- client.on-------------------");
     console.log("subscribedUsers : ", subscribedUsers);
+
+    
     console.log("client->isConnected : ", !client.isConnected());
+
+    
 
     const subscribeToAccount = async () => {
       try {
@@ -491,6 +494,7 @@ const MatrixClientProvider = () => {
             const destination = tx?.tx_json?.Destination;
             const amount = tx?.tx_json?.Amount;
             const nftId = tx?.tx_json?.NFTokenID;
+            console.log("myNftData : ", myNftData);
             const nft = myNftData
               .flatMap((user) => user.groupedNfts)
               .flatMap((group) => group.nfts)
@@ -524,46 +528,16 @@ const MatrixClientProvider = () => {
               console.log("Incoming Offer detected:", offer);
               setIncomingOffer(offer);
             }
-          } else if (type === "NFTokenCancelOffer") {
-            const offerId = extractOfferIdFromMeta(tx.meta);
-            const account = tx?.tx_json?.Account;
-            const nftId = tx?.tx_json?.NFTokenID;
-            const destination = tx?.tx_json?.Destination;
-            const owner = tx?.tx_json?.Owner;
-            const isSell =
-              (tx?.tx_json?.Flags &
-                xrpl.NFTokenCreateOfferFlags.tfSellNFToken) !==
-              0;
-            const nft = myNftData
-              .flatMap((user) => user.groupedNfts)
-              .flatMap((group) => group.nfts)
-              .find((nft) => nft.nftokenID === nftId);
-
-            if (
-              account === myOwnWalletAddress ||
-              destination === myOwnWalletAddress ||
-              owner === myOwnWalletAddress
-            ) {
-              console.log("Outgoing Sell Offer Cancellation detected");
-              setCancelledOffer({
-                offerId: offerId,
-                nftId: nft.nftokenID,
-                isSell: isSell,
-                destination: destination,
-                account: account,
-                owner: owner,
-              });
-            }
           }
         }
       }
+    };
 
-      client.on("transaction", listener);
+    client.on("transaction", listener);
 
-      // Clean up: remove listener when state changes or component unmounts
-      return () => {
-        client.off("transaction", listener);
-      };
+    // Clean up: remove listener when state changes or component unmounts
+    return () => {
+      client.off("transaction", listener);
     };
   }, [client, myNftData, myOwnWalletAddress]); // ✅ dependencies
 
@@ -709,7 +683,6 @@ const MatrixClientProvider = () => {
                     isRefreshing={isRefreshing}
                     updateUsersNFTs={updateUsersNFTs}
                     incomingOffer={incomingOffer}
-                    cancelledOffer={cancelledOffer}
                   />
                 </div>
               </motion.div>
